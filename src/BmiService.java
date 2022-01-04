61b0737c8f46ba59498859a7184e980750fdd0a5
Andrey-36 <92155825+Andrey-36@users.noreply.github.com>
2022-01-04 14:56:17 +0300

    Update BmiService.java

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,5 +1,5 @@
 public class BmiService {
-    public float calculate (float weight, float growth){
+    public float calculate (float weight, float growth) {
     float index = weight / ( growth / 100 * growth / 100) ;
     return index;
     }
