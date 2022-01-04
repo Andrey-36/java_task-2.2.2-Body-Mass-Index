8bd317d25b2d32b7f76adbb24dabf8a208992fb3
Andrey-36 <92155825+Andrey-36@users.noreply.github.com>
2022-01-04 14:55:17 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-    float weight = 74; // масса человека в килограммах
-    float growth = 172; // рост в сантиметрах
-float index = service.calculate(weight, growth);
+        
+        float weight = 74; // масса человека в килограммах
+        float growth = 172; // рост в сантиметрах
+        float index = service.calculate(weight, growth);
+        
         System.out.println(index);
-     }
     }
+}
 
